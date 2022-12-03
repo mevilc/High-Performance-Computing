382c21fd6fd97679544f099b5431f2aec69b67a3
Mevil Crasta <39920489+mevilc@users.noreply.github.com>
2022-12-03 01:34:42 -0500

    Add files via upload

---
+++ Lab 1/mem1_TLB.c
@@ -0,0 +1,81 @@
+#include "papi.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+static unsigned len = (8 << 20);
+
+void func(int * a)
+{
+	int i,j;
+	
+	
+	for(j=0; j<500; j++) {
+		for(i=0; i<len; i+=695) {
+			a[i+j]++;
+		}
+	}
+}
+
+int events[1] = {PAPI_TLB_DM}; /*PAPI_L1_DCM, PAPI_L2_DCM, PAPI_TLB_DM*/
+int eventnum = 1;
+
+int main()
+{
+	int * a;
+	int i;
+
+    int nEvents, retval;
+    int EventSet = PAPI_NULL;
+    int events[] = {PAPI_TLB_DM}; //, PAPI_L2_TCM};
+    long_long values[] = {0, 0};
+    char eventLabel[PAPI_MAX_STR_LEN];
+
+    if((retval = PAPI_library_init(PAPI_VER_CURRENT)) != PAPI_VER_CURRENT )
+    {
+        printf("\n\t  Error : PAPI Library initialization error! \n");
+        return(-1);
+    }
+
+    if((retval = PAPI_create_eventset(&EventSet)) != PAPI_OK)
+    {   printf("\n\t  Error : PAPI failed to create the Eventset\n");
+        printf("\n\t  Error string : %s  :: Error code : %d \n",PAPI_strerror(retval),retval);
+        return(-1);
+    }
+
+    nEvents = sizeof(events)/sizeof(events[0]);
+    for(i = 0; i < nEvents; i++){
+        if((retval = PAPI_add_event(EventSet, events[i])) != PAPI_OK)
+        {
+            printf("\n\t   Error : PAPI failed to add event %d\n", i);
+            printf("\n\t   Error string : %s  :: Error code : %d \n",PAPI_strerror(retval),retval);
+        }
+    }
+
+	a = (int *) malloc(len*sizeof(int));
+	PAPI_start(EventSet);
+	func(a);
+	PAPI_stop(EventSet, values);
+	free(a);
+
+	/*Print out PAPI reading*/
+    for(i = 0; i < nEvents; i++){
+        PAPI_event_code_to_name(events[i], eventLabel);
+        printf("%s:\t%lld\t", eventLabel, values[i]);
+    }
+    printf("\n");
+
+	/* Cleanup and shutdown PAPI */
+	if((retval = PAPI_cleanup_eventset(EventSet)) != PAPI_OK)
+    {
+        printf("\n\t   Error : PAPI failed to clean the events from created Eventset");
+        printf("\n\t   Error string : %s  :: Error code : %d \n",PAPI_strerror(retval),retval);
+        return(-1);
+    }
+    if((retval = PAPI_destroy_eventset(&EventSet)) != PAPI_OK)
+    {
+        printf("\n\t   Error : PAPI failed to clean the events from created Eventset");
+        printf("\n\t   Error string : %s  :: Error code : %d \n",PAPI_strerror(retval),retval);
+        return(-1);
+    }
+    PAPI_shutdown();
+}
